084d29646dfc95d67e9a7731b3c88c415354b640
Ivan Gr <of_2000@mail.ru>
2022-10-06 21:59:08 +0400

    02

---
+++ 02/Program.cs
@@ -0,0 +1,52 @@
+﻿// **Задача 48:** Задайте двумерный массив размера m на n, 
+// каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+
+using static System.Console;
+
+Clear();
+Write("Введите количество строк и количество столбцов массива (через пробел): ");
+int[] data = GetArrayFromString(ReadLine());
+int[,] matrix = GetMatrix(data[0], data[1]);
+PrintMatrix(matrix);
+
+
+
+
+
+
+int[,] GetMatrix (int rows, int columns)
+{
+    int[,] resultMatrix = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            resultMatrix[i,j] = i+j;           
+        }
+
+    }
+    return resultMatrix;
+}
+
+void PrintMatrix(int[,] inMatrix) 
+{
+    for (int i = 0; i < inMatrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < inMatrix.GetLength(1); j++)
+        {
+            Write($"{inMatrix[i,j]}\t");
+        }
+        WriteLine();
+    }
+}
+
+int [] GetArrayFromString(string input)
+{
+    string[] parametr = input.Split(" ", System.StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int [parametr.Length];
+    for (int i = 0; i < parametr.Length; i++)
+    {
+        result[i] = Convert.ToInt32(parametr[i]);
+    }
+    return result;
+}
