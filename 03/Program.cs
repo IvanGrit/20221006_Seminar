0b7d2b66f1affba14666a582133b60d908b87d40
Ivan Gr <of_2000@mail.ru>
2022-10-06 22:16:02 +0400

    03

---
+++ 03/Program.cs
@@ -0,0 +1,75 @@
+﻿// **Задача 49:** Задайте двумерный массив. Найдите элементы, 
+// у которых оба индекса чётные, и замените эти элементы на их квадраты.
+// Например, изначально массив выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Новый массив будет выглядеть вот так:
+// 1 4 7 2
+// 5 **81** 2 **9**
+// 8 4 2 4
+
+
+using static System.Console;
+
+Clear();
+Write("Введите количество строк и количество столбцов массива (через пробел): ");
+int[] data = GetArrayFromString(ReadLine());
+int[,] matrix = GetMatrix(data[0], data[1]);
+PrintMatrix(matrix);
+ModifyMatrix(matrix);
+WriteLine();
+PrintMatrix(matrix);
+
+
+
+
+
+
+int[,] GetMatrix (int rows, int columns)
+{
+    int[,] resultMatrix = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            resultMatrix[i,j] = i+j;           
+        }
+
+    }
+    return resultMatrix;
+}
+
+void PrintMatrix(int[,] inMatrix) 
+{
+    for (int i = 0; i < inMatrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < inMatrix.GetLength(1); j++)
+        {
+            Write($"{inMatrix[i,j]}\t");
+        }
+        WriteLine();
+    }
+}
+
+int [] GetArrayFromString(string input)
+{
+    string[] parametr = input.Split(" ", System.StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int [parametr.Length];
+    for (int i = 0; i < parametr.Length; i++)
+    {
+        result[i] = Convert.ToInt32(parametr[i]);
+    }
+    return result;
+}
+
+void ModifyMatrix (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if(i%2 != 0 && j%2 != 0) array[i,j] = array[i,j]*array[i,j];
+        }
+    }
+} 
